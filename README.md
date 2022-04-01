fdb9df35743adfabec190e070221efdc796df485
Phillip Stevenson <phillipestevenson@yahoo.com>
2022-03-31 23:07:32 -0700

    Add all files and image

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # password-generator
 password generator project
+
+
+![Screenshot 2022-03-31 223404](https://user-images.githubusercontent.com/101435334/161201867-12020829-9586-425f-88e4-937432789a6f.jpg)
+
+(https://user-images.githubusercontent.com/101435334/161201867-12020829-9
+586-425f-88e4-937432789a6f.jpg)
---
+++ script.js
@@ -0,0 +1,27 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword() {
+  var password = "password";
+  // TODO: add code to generate the password here
+  var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var passwordLength = 12;
+  for (var i = 0; i <= passwordLength; i++) {
+    var randomNumber = Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber +1);
+  }
+  let generatePassword = (chars, passwordLength, randomNumber ); 
+  return password;
+}
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  
+  passwordText.value = "#password";
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
